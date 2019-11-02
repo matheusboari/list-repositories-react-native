3c26e1a4d1d43d095168e6c3dbf6d2c55cee1e12
Matheus Boari <matheusboari@gmail.com>
2019-11-02 03:54:55 -0300

    Fix esling :bug:

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -26,10 +26,10 @@ export default class Main extends Component {
   };
 
   async componentDidMount() {
-    const users = await AsyncStorage.getItem('users')
+    const users = await AsyncStorage.getItem('users');
 
     if (users) {
-      this.setState({ users: JSON.parse(users) })
+      this.setState({ users: JSON.parse(users) });
     }
   }
 
